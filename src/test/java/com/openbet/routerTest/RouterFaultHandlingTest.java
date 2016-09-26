71a10d875b3a2a57b757e8c8b9a91d2e64a93aea
Yue Wang <yue.wang@openbet.com>
2016-09-26 16:03:57 +0100

    ClusterSingleton test

--- src/test/java/com/openbet/routerTest/RouterFaultHandlingTest.java
+++ src/test/java/com/openbet/routerTest/RouterFaultHandlingTest.java
@@ -12,10 +12,15 @@ import org.junit.Test;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.OneForOneStrategy;
+import akka.actor.PoisonPill;
 import akka.actor.Props;
 import akka.actor.SupervisorStrategy;
+import akka.contrib.pattern.ClusterSingletonManager;
+import akka.contrib.pattern.ClusterSingletonProxy;
+import akka.routing.FromConfig;
 import akka.routing.RoundRobinPool;
 import akka.testkit.JavaTestKit;
+import scala.collection.script.End;
 import scala.concurrent.Await;
 import scala.concurrent.duration.Duration;
 
@@ -41,10 +46,12 @@ public class RouterFaultHandlingTest {
 				  new OneForOneStrategy(1, Duration.create(10, TimeUnit.MINUTES),
 				    Collections.<Class<? extends Throwable>>singletonList(Exception.class));
         ActorRef router = system.actorOf(new RoundRobinPool(3).withSupervisorStrategy(strategy).props(Props.create(SayHelloToActor.class)), "router");
-        
+//		ActorRef router = system.actorOf(new RoundRobinPool(3).props(Props.create(SayHelloToActor.class)), "router");
+		
         assert Await.result(ask(router, "Pikachu", 5000), timeout).equals("Hello");
         assert Await.result(ask(router, "Charmander", 5000), timeout).equals("Hello");
         ask(router, new NullPointerException(), 5000);
+        Thread.sleep(5000);
         assert Await.result(ask(router, "Jigglypuff", 5000), timeout).equals("Hello");
         assert Await.result(ask(router, "Squirtle", 5000), timeout).equals("Hello");
         
@@ -54,5 +61,32 @@ public class RouterFaultHandlingTest {
 //        assert Await.result(ask(router, new ConsistentHashableEnvelope(new Entry<String>("Charmander"), "message"), 5000), timeout).equals("Hello");
 //        ask(router, new ConsistentHashableEnvelope(new Entry<Exception>(new NullPointerException()), "message"), 5000);
 //        assert Await.result(ask(router, new ConsistentHashableEnvelope(new Entry<String>("Jigglypuff"), "message"), 5000), timeout).equals("Hello");
+	}
+	
+	@Test
+	public void testSupervisorStrategyClusterSingleton() throws Exception {
+		final SupervisorStrategy strategy =
+        		new OneForOneStrategy(5, Duration.create(1, TimeUnit.MINUTES),
+        				Collections.<Class<? extends Throwable>>singletonList(Exception.class));
+		
+		system.actorOf(ClusterSingletonManager
+			.defaultProps(
+				FromConfig.getInstance()
+				.withSupervisorStrategy(strategy)
+				.props(Props.create(SayHelloToActor.class)),
+				"sayHello",
+				PoisonPill.getInstance(),
+				null
+			), "singleton"
+		);
+		
+		ActorRef router = system.actorOf(ClusterSingletonProxy.defaultProps("user/singleton/sayHello", null), "sayHelloProxy");
+        
+		assert Await.result(ask(router, "Pikachu", 5000), timeout).equals("Hello");
+        assert Await.result(ask(router, "Charmander", 5000), timeout).equals("Hello");
+        ask(router, new NullPointerException(), 5000);
+        Thread.sleep(5000);
+        assert Await.result(ask(router, "Jigglypuff", 5000), timeout).equals("Hello");
+        assert Await.result(ask(router, "Squirtle", 5000), timeout).equals("Hello");
 	}
 }
